d0d3387452893f46fbc2c8d2a1d1a950244ccd30
joshuavj888 <u193716889@byui.edu>
2023-01-18 16:13:48 -0700

    Encapsulation Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction firstFraction = new Fraction();
+        Fraction secondFraction = new Fraction(5);
+        Fraction thirdFraction = new Fraction(3, 4);
+        Fraction fourthFraction = new Fraction(1, 3);
+
+        Console.WriteLine(firstFraction.GetFractionString());
+        Console.WriteLine(firstFraction.GetDecimalValue());
+        Console.WriteLine(secondFraction.GetFractionString());
+        Console.WriteLine(secondFraction.GetDecimalValue());
+        Console.WriteLine(thirdFraction.GetFractionString());
+        Console.WriteLine(thirdFraction.GetDecimalValue());
+        Console.WriteLine(fourthFraction.GetFractionString());
+        Console.WriteLine(fourthFraction.GetDecimalValue());
     }
 }
